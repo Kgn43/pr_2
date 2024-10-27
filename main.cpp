6afccff8c5086d819b3553465d52eb43d9a07dcb
Kgn43 <kagan_43@mail.ru>
2024-10-27 06:04:24 -0400

    123

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-#include <winsock.h>
 #include <sys/socket.h>
-
+#include <mutex>
 #include "makeStructure.h"
 #include "userQuery.h"
 
@@ -9,36 +8,65 @@
 
 int main() {
 
-    int server_fd, new_socket;
-    long valread;
+    int serverSocket;
     struct sockaddr_in address;
+    int opt = 1;
     int addrlen = sizeof(address);
+    char buffer[1024] = {0};
 
-
-    //same shit
-    SOCKET ServSock = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (ServSock == INVALID_SOCKET) {
-        cout << "Error initialization socket # " << WSAGetLastError() << endl;
-        closesocket(ServSock);
-        WSACleanup();
-        return 1;
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        cerr << "Error of create socket" << endl;
+        return;
     }
-    else
-        cout << "Server socket initialization is OK" << endl;
-
-
-    // Creating socket file descriptor
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0)
-    {
-        perror("In sockets");
-        exit(EXIT_FAILURE);
+    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        cerr << "Error of setting parameters of socket" << endl;
+        return;
     }
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
+    if (bind(serverSocket, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        cerr << "Error of binding" << endl;
+        return;
+    }
+
+    if (listen(serverSocket, 1) < 0) {
+        cerr << "Error of socket listening" << endl;
+        return;
+    }
+
+
+
+    cout << "Server started" << endl;
+
+
+
+    while (true){
+        sockaddr_in clientAddress;
+        socklen_t clientSize = sizeof(clientAddress);
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientSize);
+        if(clientSocket < 0){
+            cout << "Error to connect client" << endl;
+            continue;
+        }
+
+        if(cntThreads <= MAX_CLIENTS){
+            char* clientIP = inet_ntoa(clientAddress.sin_addr);
+            cout << "Client[" << clientIP << "] was connected" << endl;
+            thread(serve_client, clientSocket, schemaName).detach();
+
+            string answer = "Successfully connected to the server";
+            send(clientSocket, answer.c_str(), answer.size(), 0);
+        }
+        else{
+            string answer = "A lot of clients now, try it later";
+            send(clientSocket, answer.c_str(), answer.size(), 0);
+            close(clientSocket);
+        }
+    }
+
+    close(serverSocket);
+}
 
 
 
